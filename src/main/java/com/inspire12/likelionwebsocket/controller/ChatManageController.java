37ef06cde32f7d6890e36a7990642d7486d75f4f
inspire12 <ox4443@naver.com>
2025-03-17 22:12:41 +0900

    feat(websocket): SYSTEM 전송 API 추가

--- src/main/java/com/inspire12/likelionwebsocket/controller/ChatManageController.java
+++ src/main/java/com/inspire12/likelionwebsocket/controller/ChatManageController.java
@@ -1,10 +1,14 @@
 package com.inspire12.likelionwebsocket.controller;
 
+import com.inspire12.likelionwebsocket.model.ChatMessage;
 import com.inspire12.likelionwebsocket.model.ChatSessionManagerMessage;
 import com.inspire12.likelionwebsocket.service.ChatManagerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,6 +19,11 @@ public class ChatManageController {
     @GetMapping("/chat/manager")
     public ChatSessionManagerMessage getChatManagerService() {
         return chatManagerService.getMessage();
+    }
+
+    @GetMapping("/chat/send")
+    public ChatSessionManagerMessage sendChatManagerService(@RequestBody ChatMessage chatMessage) throws IOException {
+        return chatManagerService.sendAllMessage(chatMessage);
     }
 
 }
--- src/main/java/com/inspire12/likelionwebsocket/service/ChatManagerService.java
+++ src/main/java/com/inspire12/likelionwebsocket/service/ChatManagerService.java
@@ -1,16 +1,39 @@
 package com.inspire12.likelionwebsocket.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inspire12.likelionwebsocket.model.ChatMessage;
 import com.inspire12.likelionwebsocket.model.ChatSessionManagerMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 public class ChatManagerService {
 
     private final ChatWebSocketHandler chatWebSocketHandler;
+    private final ObjectMapper objectMapper;
+
 
     public ChatSessionManagerMessage getMessage() {
         return new ChatSessionManagerMessage(chatWebSocketHandler.getSessions().size());
     }
+
+    public ChatSessionManagerMessage sendAllMessage(ChatMessage chatMessage) throws IOException {
+        Set<WebSocketSession> sessions = chatWebSocketHandler.getSessions();
+        TextMessage messageToSend = new TextMessage(objectMapper.writeValueAsBytes(chatMessage));
+
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(messageToSend);
+            }
+        }
+        return new ChatSessionManagerMessage(chatWebSocketHandler.getSessions().size());
+    }
+
 }
