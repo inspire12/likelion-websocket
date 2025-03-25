615f27577f990e75fe8e6d47396e4f3deded4400
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-03-25 21:45:00 +0900

    feat: 소켓 연결 유저 메시지 전송 api 구현

--- src/main/java/com/inspire12/likelionwebsocket/service/MessageService.java
+++ src/main/java/com/inspire12/likelionwebsocket/service/MessageService.java
@@ -1,12 +1,43 @@
 package com.inspire12.likelionwebsocket.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inspire12.likelionwebsocket.model.ChatMessage;
+import java.io.IOException;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 @Service
+@RequiredArgsConstructor
 public class MessageService {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+    private final ObjectMapper objectMapper;
 
     public ChatMessage createWelcomeMessage(ChatMessage chatMessage) {
         return ChatMessage.createWelcomeMessage(chatMessage.getSender());
     }
+
+    /**
+     * 클라이언트로부터 전달받은 ChatMessage 를 모든 WebSocket 세션에 브로드캐스트 합니다.
+     * @param chatMessage 전달받은 chatMessage
+     */
+    public void sendMessage(ChatMessage chatMessage) {
+        try {
+            // ChatMessage JSON 형태로 직렬화하여 TextMessage로 변환
+            TextMessage messageToSend = new TextMessage(
+                    objectMapper.writeValueAsBytes(chatMessage));
+            // 연결된 모든 WebSocket 세션 가져옴
+            Set<WebSocketSession> sessions = chatWebSocketHandler.getSessions();
+            // 모든 세션에 메시지 전송
+            for (WebSocketSession session : sessions) {
+                session.sendMessage(messageToSend);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
