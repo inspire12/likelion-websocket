f431e7fb8e9e8930eeeea4cf35a479a80549ab81
brobro332 <332bro@naver.com>
2025-03-25 22:05:09 +0900

    feat(brobro332): sendMessageToAll API 개발

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.inspire12.likelionwebsocket.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inspire12.likelionwebsocket.service.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -10,12 +11,16 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-
     private final ObjectMapper mapper;
+
+    @Bean
+    public ChatWebSocketHandler chatWebSocketHandler() {
+        return new ChatWebSocketHandler(mapper);
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatWebSocketHandler(mapper),  "/ws")
+        registry.addHandler(chatWebSocketHandler(),  "/ws")
 //                .setAllowedOrigins("http://localhost:3000");
                 .setAllowedOrigins("*");
     }
