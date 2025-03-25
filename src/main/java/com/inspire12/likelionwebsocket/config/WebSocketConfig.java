4e2f502429cedf02978abbcdd6e05aef085d4e16
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2025-03-25 23:56:22 +0900

    feat(main) : 수정

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSocket
+@EnableWebSocket // Spring WebSocket을 활성화하는 어노테이션
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final ChatWebSocketHandler chatWebSocketHandler;
+    private final ChatWebSocketHandler chatWebSocketHandler; // WebSocket 핸들러 등록
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "/ws")
-                .setAllowedOrigins("*");
+        registry.addHandler(chatWebSocketHandler, "/ws") // "/ws" 경로로 들어오는 WebSocket 요청을 chatWebSockeHandler가 처리하도록 설정
+                .setAllowedOrigins("*"); // 모든 도메인에서 WebSocket 접속을 허용
     }
 }
