742f0243ec7393c1f9c2bc7609f366d4177f1846
inspire12 <ox4443@naver.com>
2025-03-26 12:51:44 +0900

    feat(websocket-session-holder): 세션 디버그 추가

--- src/main/java/com/inspire12/likelionwebsocket/controller/ChatAdminController.java
+++ src/main/java/com/inspire12/likelionwebsocket/controller/ChatAdminController.java
@@ -1,24 +1,17 @@
 package com.inspire12.likelionwebsocket.controller;
 
 import com.inspire12.likelionwebsocket.model.ChatMessage;
-import com.inspire12.likelionwebsocket.service.ChatWebSocketHandler;
 import com.inspire12.likelionwebsocket.service.MessageService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
-
-import java.util.Set;
 
 @RestController
-public class ChatController {
+public class ChatAdminController {
 
     private final MessageService messageService;
 
-    public ChatController(MessageService messageService) {
+    public ChatAdminController(MessageService messageService) {
         this.messageService = messageService;
     }
 
--- src/main/java/com/inspire12/likelionwebsocket/holder/WebSocketSessionHolder.java
+++ src/main/java/com/inspire12/likelionwebsocket/holder/WebSocketSessionHolder.java
@@ -1,5 +1,6 @@
 package com.inspire12.likelionwebsocket.holder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashSet;
@@ -7,14 +8,19 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 
+@Slf4j
 public class WebSocketSessionHolder {
     public static Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
 
     public static void addSession(WebSocketSession session) {
+        if (sessions.size() ==  999) {
+            log.info("sessions size is {}", sessions.size());
+        }
         sessions.add(session);
     }
 
     public static Set<WebSocketSession> getSessions() {
         return sessions;
     }
+
 }
