d1e60efea5e2a357ba76d8dc84dff3c734e1a81f
CHOIIS829 <cth7097@gmail.com>
2025-03-26 13:58:54 +0900

    전체 메세지 구현

---
+++ src/main/java/com/inspire12/likelionwebsocket/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.inspire12.likelionwebsocket.controller;
+
+import com.inspire12.likelionwebsocket.model.ChatMessage;
+import com.inspire12.likelionwebsocket.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ChatController {
+
+    private final MessageService messageService;
+
+    @PostMapping("/call")
+    public ChatMessage call(@RequestBody ChatMessage chatMessage){
+        return messageService.sendMessage(chatMessage);
+    }
+
+}
