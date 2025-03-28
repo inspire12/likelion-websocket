43ccd1f4977085712e8b7fd3db93c8b2f9b5a999
Dingwoonee <slm0523@naver.com>
2025-03-28 10:51:42 +0900

    feat(feature/websocket-practice): WebSocket 구현
    - WebSocket 의존성 추가
    - WebSocketHandler 구현
    - WebSocketConfig 구현

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -4,11 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inspire12.likelionwebsocket.service.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
     private final ObjectMapper mapper;
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "ws")
+                .setAllowedOrigins("*");
+    }
 }
 
