22b4980bc041ff519d322c72ef005efa15c4d45f
minseoBae <baejjae8218@gmail.com>
2025-03-28 10:45:22 +0900

    feat(websocker-practice) : 배민서 작업 (완료)

--- src/main/java/com/inspire12/likelionwebsocket/service/ChatWebSocketHandler.java
+++ src/main/java/com/inspire12/likelionwebsocket/service/ChatWebSocketHandler.java
@@ -15,8 +15,38 @@ import java.security.Principal;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-public class ChatWebSocketHandler {
+@Service
+@RequiredArgsConstructor
+public class ChatWebSocketHandler extends TextWebSocketHandler {
     // 연결된 모든 세션을 저장할 스레드 안전한 Set
+    private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+    private final ObjectMapper objectMapper;
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        ChatMessage chatMessage = objectMapper.readValue(message.getPayload(), ChatMessage.class);
+        TextMessage messageToSend = message;
+
+        if(chatMessage.getType() == ChatMessage.MessageType.JOIN) {
+            ChatMessage welcomeMessage = ChatMessage.createWelcomeMessage(chatMessage.getSender());
+            messageToSend = new TextMessage(objectMapper.writeValueAsString(welcomeMessage));
+        }
+
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(messageToSend);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
 }
 
