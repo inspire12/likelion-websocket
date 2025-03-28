697afb3c75a16813153dd0cfe230768ba88f3338
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2025-03-28 12:13:17 +0900

    feat(simple-broker-practice) 작업 완료

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.inspire12.likelionwebsocket.config;
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -15,17 +16,39 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 import java.util.List;
 
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        ObjectMapper mapper = new ObjectMapper();
-        // unknown enum 값이 들어오면 mixin에서 지정한 default(StompCommand.SEND)를 사용하도록 설정
-        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
-        converter.setObjectMapper(mapper);
-        messageConverters.add(converter);
-        // false를 리턴하면 기본 변환기도 함께 등록되지만 여기서는 커스텀 변환기만 사용하도록 합니다.
-        return false;
+    private final HttpMessageConverters messageConverters;
+
+    public WebSocketConfig(HttpMessageConverters messageConverters) {
+        this.messageConverters = messageConverters;
     }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+    }
+
+        @Override
+        public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+            MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+            ObjectMapper mapper = new ObjectMapper();
+            // unknown enum 값이 들어오면 mixin에서 지정한 default(StompCommand.SEND)를 사용하도록 설정
+            mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
+            converter.setObjectMapper(mapper);
+            messageConverters.add(converter);
+            // false를 리턴하면 기본 변환기도 함께 등록되지만 여기서는 커스텀 변환기만 사용하도록 합니다.
+            return false;
+        }
 }
+
+
