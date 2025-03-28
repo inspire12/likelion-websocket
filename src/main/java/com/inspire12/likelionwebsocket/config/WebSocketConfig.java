a5c1e47ff3f3082450c54173bf03d04bb855c758
ooyniz <ooyniz@gmail.com>
2025-03-28 10:47:14 +0900

    feat(websocket-practive): websocket

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -1,14 +1,23 @@
 package com.inspire12.likelionwebsocket.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.inspire12.likelionwebsocket.service.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
     private final ObjectMapper mapper;
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // endpoint : /ws
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
+    }
 }
 
--- src/main/java/com/inspire12/likelionwebsocket/service/ChatWebSocketHandler.java
+++ src/main/java/com/inspire12/likelionwebsocket/service/ChatWebSocketHandler.java
@@ -4,19 +4,50 @@ package com.inspire12.likelionwebsocket.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inspire12.likelionwebsocket.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.PongMessage;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.security.Principal;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-public class ChatWebSocketHandler {
+@Component
+@RequiredArgsConstructor
+public class ChatWebSocketHandler extends TextWebSocketHandler{
+
+    private final ObjectMapper objectMapper;
     // 연결된 모든 세션을 저장할 스레드 안전한 Set
+    private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);  // WebSocketSession 을 Set에 추가.
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        ChatMessage chatMessage = objectMapper.readValue(message.getPayload(), ChatMessage.class);
+        TextMessage messageToSend = message;
+        if (chatMessage.getType() == ChatMessage.MessageType.JOIN) {
+            ChatMessage welcomeMessage = ChatMessage.createWelcomeMessage(chatMessage.getSender());
+            messageToSend = new TextMessage(objectMapper.writeValueAsBytes(welcomeMessage));
+        }
+
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(messageToSend);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        sessions.remove(session);
+    }
 
 }
 
