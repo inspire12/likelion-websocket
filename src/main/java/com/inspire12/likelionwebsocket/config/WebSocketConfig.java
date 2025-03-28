01e7500a9e037e20d202cf467ae2d551af42367c
brobro332 <332bro@naver.com>
2025-03-28 22:00:19 +0900

    feat(websocket-practice): 김진형 작업 완료

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -1,12 +1,23 @@
 package com.inspire12.likelionwebsocket.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inspire12.likelionwebsocket.service.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
     private final ObjectMapper mapper;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatWebSocketHandler(mapper), "/ws")
+                .setAllowedOrigins("*");
+    }
 }
 
---
+++ src/main/java/com/inspire12/likelionwebsocket/service/ChatWebSocketHandler.java
@@ -0,0 +1,49 @@
+package com.inspire12.likelionwebsocket.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inspire12.likelionwebsocket.model.ChatMessage;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+    @Getter
+    private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+    private final ObjectMapper objectMapper;
+
+    public ChatWebSocketHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        ChatMessage chatMessage = objectMapper.readValue(message.getPayload(), ChatMessage.class);
+        TextMessage messageToSend = message;
+
+        if (chatMessage.getType() == ChatMessage.MessageType.JOIN) {
+            ChatMessage welcomeMessage = ChatMessage.createWelcomeMessage(chatMessage.getSender());
+            messageToSend = new TextMessage(objectMapper.writeValueAsBytes(welcomeMessage));
+        }
+
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) webSocketSession.sendMessage(messageToSend);
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
