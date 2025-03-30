47c3d1fe705ac94b052fc0f97eead21b1c71f42b
Azamman327 <wjsdbdud327@naver.com>
2025-03-30 17:14:39 +0900

    feat(feature/websocket-practice: add system message

--- src/main/java/com/inspire12/likelionwebsocket/service/MessageService.java
+++ src/main/java/com/inspire12/likelionwebsocket/service/MessageService.java
@@ -1,12 +1,42 @@
 package com.inspire12.likelionwebsocket.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inspire12.likelionwebsocket.model.ChatMessage;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.Set;
 
 @Service
+@AllArgsConstructor
 public class MessageService {
+
+    private final ObjectMapper objectMapper;
+    private final ChatWebSocketHandler chatWebSocketHandler;
 
     public ChatMessage createWelcomeMessage(ChatMessage chatMessage) {
         return ChatMessage.createWelcomeMessage(chatMessage.getSender());
+    }
+
+    public void createSystemMessage(ChatMessage message) {
+        Set<WebSocketSession> sessions = chatWebSocketHandler.sessions;
+
+        try {
+            TextMessage messageToSend = new TextMessage(objectMapper.writeValueAsBytes(message));
+            for (WebSocketSession webSocketSession : sessions) {
+                if (webSocketSession.isOpen()) {
+                    webSocketSession.sendMessage(messageToSend);
+                }
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
